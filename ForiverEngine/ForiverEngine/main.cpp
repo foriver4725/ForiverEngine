8d5a4021a3e6d0c7847de77d796f41f8d0eeb1b0
foriver4725 <hrt67@icloud.com>
2025-12-09 15:18:09 +0900

    順番を変える

--- ForiverEngine/ForiverEngine/main.cpp
+++ ForiverEngine/ForiverEngine/main.cpp
@@ -18,10 +18,7 @@ BEGIN_INITIALIZE(L"DX12Sample", L"DX12 テスト", hwnd, WindowWidth, WindowHeig
 	const auto [factory, device, commandAllocater, commandList, commandQueue, swapChain, descriptorHeapRTV]
 		= D3D12BasicFlow::CreateStandardObjects(hwnd, WindowWidth, WindowHeight);
 
-	// ビューポートとシザー矩形
-	const ViewportScissorRect viewportScissorRect
-		= ViewportScissorRect::CreateFullSized(WindowWidth, WindowHeight);
-
+	// 頂点データ
 	const std::vector<VertexData> vertices =
 	{
 		{{ -0.4f, -0.7f, 0, 1 }, {0.0f, 1.0f}},
@@ -37,18 +34,20 @@ BEGIN_INITIALIZE(L"DX12Sample", L"DX12 テスト", hwnd, WindowWidth, WindowHeig
 		2, 1, 3,
 	};
 
-	const auto [vertexBufferView, indexBufferView]
-		= D3D12BasicFlow::CreateVertexAndIndexBufferViews(device, vertices, indices);
-
-	// シェーダーをロード
-	const auto [shaderVS, shaderPS]
-		= D3D12BasicFlow::CompileShader_VS_PS("./shaders/Basic.hlsl");
-
-	// 頂点レイアウトを作成
+	// 頂点レイアウト
 	const std::vector<VertexLayout> vertexLayouts =
 	{
 		{ "POSITION", Format::RGBA_F32 },
 	};
+
+	const auto [vertexBufferView, indexBufferView]
+		= D3D12BasicFlow::CreateVertexAndIndexBufferViews(device, vertices, indices);
+
+	const auto [shaderVS, shaderPS]
+		= D3D12BasicFlow::CompileShader_VS_PS("./shaders/Basic.hlsl");
+
+	const ViewportScissorRect viewportScissorRect
+		= ViewportScissorRect::CreateFullSized(WindowWidth, WindowHeight);
 
 	const auto [rootSignature, graphicsPipelineState]
 		= D3D12BasicFlow::CreateRootSignatureAndGraphicsPipelineState(device, shaderVS, shaderPS, vertexLayouts, FillMode::Solid, CullMode::None);
