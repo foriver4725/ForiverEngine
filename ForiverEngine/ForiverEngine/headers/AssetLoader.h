e5b903f983cb27b953017204cc6aae877e1de688
foriver4725 <hrt67@icloud.com>
2025-12-11 23:16:03 +0900

    DirectXTexを持ってきた

---
+++ ForiverEngine/ForiverEngine/headers/AssetLoader.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+namespace ForiverEngine
+{
+	class AssetLoader final
+	{
+	public:
+		AssetLoader() = delete;
+		~AssetLoader() = delete;
+	};
+}
---
+++ ForiverEngine/ForiverEngine/sources/AssetLoader.cpp
@@ -0,0 +1,13 @@
+﻿#include "../headers/AssetLoader.h"
+
+#include <DirectXTex.h>
+
+#if _DEBUG
+#pragma comment(lib, "DirectXTex_x64_Debug.lib")
+#else
+#pragma comment(lib, "DirectXTex_x64_Release.lib")
+#endif
+
+namespace ForiverEngine
+{
+}
