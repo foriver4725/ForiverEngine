7092ca4779b761b860750c6ae9f0ee5fab34edad
foriver4725 <hrt67@icloud.com>
2025-12-15 12:29:53 +0900

    立方体の辺の長さを1にした

--- ForiverEngine/ForiverEngine/scripts/common/Component/Mesh/Mesh.h
+++ ForiverEngine/ForiverEngine/scripts/common/Component/Mesh/Mesh.h
@@ -18,47 +18,47 @@ namespace ForiverEngine
 				.vertices =
 				{
 					// Up
-					{ Vector4(-1, 1, -1), Vector2(0.25f, 0.50f), Vector3(0, 1, 0)},
-					{ Vector4(-1, 1, 1), Vector2(0.25f, 0.25f), Vector3(0, 1, 0)},
-					{ Vector4(1, 1, -1), Vector2(0.50f, 0.50f), Vector3(0, 1, 0)},
-					{ Vector4(1, 1, 1), Vector2(0.50f, 0.25f), Vector3(0, 1, 0)},
+					{ Vector4(-0.5f, +0.5f, -0.5f), Vector2(0.25f, 0.50f), Vector3::Up() },
+					{ Vector4(-0.5f, +0.5f, +0.5f), Vector2(0.25f, 0.25f), Vector3::Up() },
+					{ Vector4(+0.5f, +0.5f, -0.5f), Vector2(0.50f, 0.50f), Vector3::Up() },
+					{ Vector4(+0.5f, +0.5f, +0.5f), Vector2(0.50f, 0.25f), Vector3::Up() },
 
 					// Down
-					{ Vector4(-1, -1, 1), Vector2(1.00f, 0.25f), Vector3(0, -1, 0)},
-					{ Vector4(-1, -1, -1), Vector2(1.00f, 0.50f), Vector3(0, -1, 0)},
-					{ Vector4(1, -1, 1), Vector2(0.75f, 0.25f), Vector3(0, -1, 0)},
-					{ Vector4(1, -1, -1), Vector2(0.75f, 0.50f), Vector3(0, -1, 0)},
+					{ Vector4(-0.5f, -0.5f, +0.5f), Vector2(1.00f, 0.25f), Vector3::Down() },
+					{ Vector4(-0.5f, -0.5f, -0.5f), Vector2(1.00f, 0.50f), Vector3::Down() },
+					{ Vector4(+0.5f, -0.5f, +0.5f), Vector2(0.75f, 0.25f), Vector3::Down() },
+					{ Vector4(+0.5f, -0.5f, -0.5f), Vector2(0.75f, 0.50f), Vector3::Down() },
 
 					// Right
-					{ Vector4(1, -1, -1), Vector2(0.75f, 0.50f), Vector3(1, 0, 0)},
-					{ Vector4(1, 1, -1), Vector2(0.50f, 0.50f), Vector3(1, 0, 0)},
-					{ Vector4(1, -1, 1), Vector2(0.75f, 0.25f), Vector3(1, 0, 0)},
-					{ Vector4(1, 1, 1), Vector2(0.50f, 0.25f), Vector3(1, 0, 0)},
+					{ Vector4(+0.5f, -0.5f, -0.5f), Vector2(0.75f, 0.50f), Vector3::Right() },
+					{ Vector4(+0.5f, +0.5f, -0.5f), Vector2(0.50f, 0.50f), Vector3::Right() },
+					{ Vector4(+0.5f, -0.5f, +0.5f), Vector2(0.75f, 0.25f), Vector3::Right() },
+					{ Vector4(+0.5f, +0.5f, +0.5f), Vector2(0.50f, 0.25f), Vector3::Right() },
 
 					// Left
-					{ Vector4(-1, -1, 1), Vector2(0.00f, 0.25f), Vector3(-1, 0, 0)},
-					{ Vector4(-1, 1, 1), Vector2(0.25f, 0.25f), Vector3(-1, 0, 0)},
-					{ Vector4(-1, -1, -1), Vector2(0.00f, 0.50f), Vector3(-1, 0, 0)},
-					{ Vector4(-1, 1, -1), Vector2(0.25f, 0.50f), Vector3(-1, 0, 0)},
+					{ Vector4(-0.5f, -0.5f, +0.5f), Vector2(0.00f, 0.25f), Vector3::Left() },
+					{ Vector4(-0.5f, +0.5f, +0.5f), Vector2(0.25f, 0.25f), Vector3::Left() },
+					{ Vector4(-0.5f, -0.5f, -0.5f), Vector2(0.00f, 0.50f), Vector3::Left() },
+					{ Vector4(-0.5f, +0.5f, -0.5f), Vector2(0.25f, 0.50f), Vector3::Left() },
 
 					// Forward
-					{ Vector4(1, -1, 1), Vector2(0.50f, 0.00f), Vector3(0, 0, 1)},
-					{ Vector4(1, 1, 1), Vector2(0.50f, 0.25f), Vector3(0, 0, 1)},
-					{ Vector4(-1, -1, 1), Vector2(0.25f, 0.00f), Vector3(0, 0, 1)},
-					{ Vector4(-1, 1, 1), Vector2(0.25f, 0.25f), Vector3(0, 0, 1)},
+					{ Vector4(+0.5f, -0.5f, +0.5f), Vector2(0.50f, 0.00f), Vector3::Forward() },
+					{ Vector4(+0.5f, +0.5f, +0.5f), Vector2(0.50f, 0.25f), Vector3::Forward() },
+					{ Vector4(-0.5f, -0.5f, +0.5f), Vector2(0.25f, 0.00f), Vector3::Forward() },
+					{ Vector4(-0.5f, +0.5f, +0.5f), Vector2(0.25f, 0.25f), Vector3::Forward() },
 
 					// Backward
-					{ Vector4(-1, -1, -1), Vector2(0.25f, 0.75f), Vector3(0, 0, -1)},
-					{ Vector4(-1, 1, -1), Vector2(0.25f, 0.50f), Vector3(0, 0, -1)},
-					{ Vector4(1, -1, -1), Vector2(0.50f, 0.75f), Vector3(0, 0, -1)},
-					{ Vector4(1, 1, -1), Vector2(0.50f, 0.50f), Vector3(0, 0, -1)},
+					{ Vector4(-0.5f, -0.5f, -0.5f), Vector2(0.25f, 0.75f), Vector3::Backward() },
+					{ Vector4(-0.5f, +0.5f, -0.5f), Vector2(0.25f, 0.50f), Vector3::Backward() },
+					{ Vector4(+0.5f, -0.5f, -0.5f), Vector2(0.50f, 0.75f), Vector3::Backward() },
+					{ Vector4(+0.5f, +0.5f, -0.5f), Vector2(0.50f, 0.50f), Vector3::Backward() },
 				},
 
 				.indices =
 				{
-					0, 1, 2, 2, 1, 3,       // Up
-					4, 5, 6, 6, 5, 7,       // Down
-					8, 9, 10, 10, 9, 11,    // Right
+					 0,  1,  2,  2,  1,  3, // Up
+					 4,  5,  6,  6,  5,  7, // Down
+					 8,  9, 10, 10,  9, 11, // Right
 					12, 13, 14, 14, 13, 15, // Left
 					16, 17, 18, 18, 17, 19, // Forward
 					20, 21, 22, 22, 21, 23, // Backward
