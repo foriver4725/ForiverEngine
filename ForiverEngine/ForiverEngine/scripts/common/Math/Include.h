362e474b03a8b65d9da0f77d3c08d8ed0bd55c83
foriver4725 <hrt67@icloud.com>
2026-01-02 04:38:03 +0900

    乱数クラスを作成

--- ForiverEngine/ForiverEngine/scripts/common/Math/Include.h
+++ ForiverEngine/ForiverEngine/scripts/common/Math/Include.h
@@ -3,5 +3,6 @@
 #include "./Defines.h"
 #include "./LinearAlgebra/Include.h"
 #include "./Color.h"
+#include "./Random.h"
 #include "./BitFlag.h"
 #include "./Noise.h"
---
+++ ForiverEngine/ForiverEngine/scripts/common/Math/Random.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <scripts/common/IncludeInternal.h>
+#include <scripts/common/Math/Defines.h>
+
+#include <random>
+#include <cstdint>
+#include <concepts>
+
+namespace ForiverEngine
+{
+	class Random final
+	{
+	public:
+		DELETE_DEFAULT_METHODS(Random);
+
+		/// <summary>
+		/// シード値を変更 (呼び出されたスレッドでのみ有効)
+		/// </summary>
+		static void SetSeed(std::uint32_t seed) { engine.seed(static_cast<unsigned int>(seed)); }
+
+		/// <summary>
+		/// 乱数生成. [min, max] の整数
+		/// </summary>
+		template <std::integral TInt>
+		static TInt Range(TInt min, TInt max) { return std::uniform_int_distribution<TInt>{min, max}(engine); }
+
+		/// <summary>
+		/// 乱数生成. [min, max) の実数
+		/// </summary>
+		template <std::floating_point TFloat>
+		static TFloat Range(TFloat min, TFloat max) { return std::uniform_real_distribution<TFloat>{min, max}(engine); }
+
+	private:
+		// 乱数生成器
+		inline static thread_local std::mt19937 engine = std::mt19937(std::random_device{}());
+	};
+}
