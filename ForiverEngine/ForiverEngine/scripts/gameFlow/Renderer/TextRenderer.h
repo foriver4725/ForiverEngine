8cc78d2eb4079fc691b9b87a732b4ab560317b55
foriver4725 <hrt67@icloud.com>
2026-01-15 19:19:09 +0900

    変数名修正

--- ForiverEngine/ForiverEngine/scripts/gameFlow/Renderer/TextRenderer.h
+++ ForiverEngine/ForiverEngine/scripts/gameFlow/Renderer/TextRenderer.h
@@ -40,24 +40,24 @@ namespace ForiverEngine
 			data = TextUIData::CreateEmpty(windowSize / TextUIData::FontTextureTextLength);
 
 			// t1 (フォントテクスチャ)
-			const Texture sr1Texture = D3D12Utils::LoadTexture({ "assets/font.png" });
-			const GraphicsBuffer sr1 = D3D12Utils::InitSR(device, commandList, commandQueue, commandAllocator, sr1Texture);
+			const Texture sr1Metadata = D3D12Utils::LoadTexture({ "assets/font.png" });
+			const GraphicsBuffer sr1 = D3D12Utils::InitSR(device, commandList, commandQueue, commandAllocator, sr1Metadata);
 
 			// t2 (データをテクスチャとしてアップロードする)
-			const Texture sr2Texture = data.CreateTexture();
-			const GraphicsBuffer sr2 = D3D12Utils::InitSR(device, commandList, commandQueue, commandAllocator, sr2Texture);
+			const Texture sr2Metadata = data.CreateTexture();
+			const GraphicsBuffer sr2 = D3D12Utils::InitSR(device, commandList, commandQueue, commandAllocator, sr2Metadata);
 
 			// b0
 			const CBData0 cbData0 =
 			{
-				.FontTextureSize = { static_cast<std::uint32_t>(sr1Texture.width), static_cast<std::uint32_t>(sr1Texture.height) },
-				.TextUIDataSize = { static_cast<std::uint32_t>(sr2Texture.width), static_cast<std::uint32_t>(sr2Texture.height) },
+				.FontTextureSize = { static_cast<std::uint32_t>(sr1Metadata.width), static_cast<std::uint32_t>(sr1Metadata.height) },
+				.TextUIDataSize = { static_cast<std::uint32_t>(sr2Metadata.width), static_cast<std::uint32_t>(sr2Metadata.height) },
 				.InvalidFontTextureIndex = static_cast<std::uint32_t>(Text::InvalidFontTextureIndex),
 				.FontTextureTextLength = static_cast<std::uint32_t>(TextUIData::FontTextureTextLength),
 			};
 			const GraphicsBuffer cb0 = D3D12Utils::InitCB(device, cbData0);
 
-			Base::Init(device, windowSize, { cb0 }, { { sr1, sr1Texture }, { sr2, sr2Texture } }, "./shaders/Text.hlsl");
+			Base::Init(device, windowSize, { cb0 }, { { sr1, sr1Metadata }, { sr2, sr2Metadata } }, "./shaders/Text.hlsl");
 		}
 
 		/// <summary>
